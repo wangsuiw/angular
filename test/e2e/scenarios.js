7a4fc460df178ce65758652bb10e10eb6b268823
roy <alexl@evilnut.ca>
2015-03-24 18:29:21 -0700

    finished step 4

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -4,18 +4,10 @@
 
 describe('PhoneCat App', function() {
 
-  it('should redirect index.html to index.html#/phones', function() {
-    browser.get('app/index.html');
-    browser.getLocationAbsUrl().then(function(url) {
-        expect(url.split('#')[1]).toBe('/phones');
-      });
-  });
-
-
   describe('Phone list view', function() {
 
     beforeEach(function() {
-      browser.get('app/index.html#/phones');
+      browser.get('app/index.html');
     });
 
 
@@ -24,16 +16,15 @@ describe('PhoneCat App', function() {
       var phoneList = element.all(by.repeater('phone in phones'));
       var query = element(by.model('query'));
 
-      expect(phoneList.count()).toBe(20);
+      expect(phoneList.count()).toBe(3);
 
       query.sendKeys('nexus');
       expect(phoneList.count()).toBe(1);
 
       query.clear();
       query.sendKeys('motorola');
-      expect(phoneList.count()).toBe(8);
+      expect(phoneList.count()).toBe(2);
     });
-
 
     it('should be possible to control phone order via the drop down select box', function() {
 
@@ -60,42 +51,6 @@ describe('PhoneCat App', function() {
         "Motorola XOOM\u2122 with Wi-Fi"
       ]);
     });
-
-
-    it('should render phone specific links', function() {
-      var query = element(by.model('query'));
-      query.sendKeys('nexus');
-      element.all(by.css('.phones li a')).first().click();
-      browser.getLocationAbsUrl().then(function(url) {
-        expect(url.split('#')[1]).toBe('/phones/nexus-s');
-      });
-    });
-  });
-
-
-  describe('Phone detail view', function() {
-
-    beforeEach(function() {
-      browser.get('app/index.html#/phones/nexus-s');
-    });
-
-
-    it('should display nexus-s page', function() {
-      expect(element(by.binding('phone.name')).getText()).toBe('Nexus S');
-    });
-
-
-    it('should display the first phone image as the main phone image', function() {
-      expect(element(by.css('img.phone.active')).getAttribute('src')).toMatch(/img\/phones\/nexus-s.0.jpg/);
-    });
-
-
-    it('should swap main image if a thumbnail image is clicked on', function() {
-      element(by.css('.phone-thumbs li:nth-child(3) img')).click();
-      expect(element(by.css('img.phone.active')).getAttribute('src')).toMatch(/img\/phones\/nexus-s.2.jpg/);
-
-      element(by.css('.phone-thumbs li:nth-child(1) img')).click();
-      expect(element(by.css('img.phone.active')).getAttribute('src')).toMatch(/img\/phones\/nexus-s.0.jpg/);
-    });
+    
   });
 });
